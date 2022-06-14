52ad8a3916a8facc6d5ee4b614752f32c831383c
another parsa <anotherparsa@gmail.com>
2022-06-14 11:33:25 +0430

    word counter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from crypt import methods
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +7,19 @@ app = Flask(__name__)
 def home():
     return render_template("home/home.html")
 
+@app.route("/result", methods=["POST"])
+def result():
+    words = request.form.get("text").split()
+    wordCounter = dict()
+    for word in words:
+        if word in wordCounter:
+            wordCounter[word] += 1
+        else:
+            wordCounter[word] = 1
+    wordNumber = len(wordCounter.keys())
+    return render_template("result/result.html", wordNumber=wordNumber, wordCounter=wordCounter, words=list(set(words)))
+
+    
 
 
 
